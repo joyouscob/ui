ba3f812e3fc199ac08983abd2db00ba4486b2fef
Michael F <tehfreak@awesome39.com>
2013-03-06 20:43:23 +0300

    добавил директиву блока Treeview

---
+++ components/awesome-directives/b-treeview/index.js
@@ -0,0 +1,22 @@
+/**
+ * Treeview block
+ */
+angular.module('awesome.directives').directive('bTreeview', function factory($compile) {
+    return {
+        restrict: 'E',
+        scope: {data:'=', parent:'='},
+        compile: function (tE, tA) {
+            return function (scope, iE, iA) {
+                var tpl= '<a href="#">{{data.title}}</a>';
+                if (angular.isArray(scope.data.nodes)) {
+                    tpl += '<ul><li ng-repeat="node in data.nodes"><b-treeview data="node" parent="data.nodes"/></li></ul>';
+                }
+                iE.replaceWith(
+                    $compile(
+                        angular.element(tpl)
+                    )(scope)
+                )
+            };
+        },
+    };
+})
